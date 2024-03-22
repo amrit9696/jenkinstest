62da7c86677ca487690d2f649d51c726cd83ae56
Amrit Singh <amrit4qa@gmail.com>
2024-03-21 22:50:01 -0400

    added new test script 4

---
+++ src/test/java/test/Script4.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Script4 {
+
+	
+	@Test
+	public static void tester() {
+		System.out.println("I am tester1");
+
+	}
+	
+	public static void tester2() {
+		System.out.println("I am tester2");
+
+	}
+
+
+}
