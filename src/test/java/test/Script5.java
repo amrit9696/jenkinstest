c28acd99f4f7fe0e75da6461499a9d54334596a3
Amrit Singh <amrit4qa@gmail.com>
2024-03-23 19:41:52 -0400

    added sanity test

---
+++ src/test/java/test/Script5.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Script5 {
+
+	
+	@Test
+	public static void tester() {
+		System.out.println("I am tester1000");
+
+	}
+	
+	public static void tester2() {
+		System.out.println("I am tester1001");
+
+	}
+
+
+}
