c43a54cb18fc36cb684e544881deb94b792f1738
Amrit Singh <amrit4qa@gmail.com>
2024-03-23 22:28:01 -0400

    added changes to the script 4 by local user

--- src/test/java/test/Script4.java
+++ src/test/java/test/Script4.java
@@ -9,8 +9,7 @@ public class Script4 {
 	public static void tester() {
 		System.out.println("I am tester1");
 		System.out.println("I am fruit from remote");
-		
-
+		System.out.println("I am fruit from local");
 	}
 	
 	public static void tester2() {
